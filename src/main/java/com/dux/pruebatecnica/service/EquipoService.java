835df17b85084ad16e7d0ad93119ad06d78e31fb
augustoba <yo_augusto15@hotmail.com>
2024-03-21 13:00:35 -0300

    creando servicios crearEquipo, listarEquipos,findbyid

--- src/main/java/com/dux/pruebatecnica/service/EquipoService.java
+++ src/main/java/com/dux/pruebatecnica/service/EquipoService.java
@@ -1,4 +1,16 @@
 package com.dux.pruebatecnica.service;
 
+import com.dux.pruebatecnica.model.Equipo;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EquipoService {
+    Optional<Equipo> findEquipoById(Integer equipoId);
+
+    Equipo crearEquipo(Equipo equipo);
+
+    List<Equipo> listarEquipos();
+
+    Equipo encontrarPorNombre(String nombre);
 }
