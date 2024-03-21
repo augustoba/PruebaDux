825073397653c5220e34b57812e0391a9cc472bf
augustoba <yo_augusto15@hotmail.com>
2024-03-21 13:01:59 -0300

    agregando controladores crearequipos,  listar equipos

--- src/main/java/com/dux/pruebatecnica/controller/EquipoController.java
+++ src/main/java/com/dux/pruebatecnica/controller/EquipoController.java
@@ -1,7 +1,46 @@
 package com.dux.pruebatecnica.controller;
 
-import org.springframework.stereotype.Controller;
+import com.dux.pruebatecnica.model.Equipo;
+import com.dux.pruebatecnica.service.EquipoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class EquipoController {
-}
+    private final EquipoService equipoService;
+
+    public EquipoController(EquipoService equipoService) {
+        this.equipoService = equipoService;
+    }
+
+    @PostMapping("/equipos")
+    public ResponseEntity<Object> crearEquipo(@Valid  @RequestBody Equipo equipo,  BindingResult result) {
+        System.out.println(equipo.getLiga() + equipo.getPais() + "asdsad");
+        if (result.hasErrors()) {
+            List<String> errorList = result.getFieldErrors().stream().map(error -> error.getDefaultMessage()).toList();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorList);
+        }
+        try {
+            Equipo equipoObj = equipoService.crearEquipo(equipo);
+            return ResponseEntity.status(HttpStatus.CREATED).body(equipoObj);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La solicitud es inválida");
+        }
+    }
+
+
+    @GetMapping("/equipos")
+    public ResponseEntity<Object> listarEquipos() {
+            return ResponseEntity.status(HttpStatus.OK).body(equipoService.listarEquipos());
+        }
+
+    }
+
--- src/main/java/com/dux/pruebatecnica/repository/EquipoRepository.java
+++ src/main/java/com/dux/pruebatecnica/repository/EquipoRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EquipoRepository extends JpaRepository<Equipo,Integer> {
 
+    Equipo findByNombre(String nombre);
 }
