4692ad11e2e8278a29db1132803f0fe1a7eddce6
augustoba <yo_augusto15@hotmail.com>
2024-03-26 09:58:41 -0300

    agregando coleccion de postman

--- src/main/java/com/dux/pruebatecnica/Security/SecurityConfig.java
+++ src/main/java/com/dux/pruebatecnica/Security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.dux.pruebatecnica.Security;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
